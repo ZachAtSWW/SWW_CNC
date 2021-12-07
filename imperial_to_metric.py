e1216b589b9e91dfaa78c564c84661bd91430f0c
Zach Owens <zach@swisswoodworking.com>
2021-12-07 12:34:19 -0800

    added CHENG_TOCHA_OFFICE_STORAGE csvs and changed imperial to metric py to generate it

--- imperial_to_metric.py
+++ imperial_to_metric.py
@@ -3,7 +3,7 @@ import csv
 """CSV cannot contain special characters '#', '&', etc"""
 
 # Name of csv being imported without '.csv'
-naming_config = 'Holmby_Master_Bath'
+naming_config = 'CHENG_TOCHA_OFFICE_STORAGE'
 # .bSolid file referenced by bNest for tooling information
 program_name =  'RECTparam.bSolid'
 # Adding .csv
