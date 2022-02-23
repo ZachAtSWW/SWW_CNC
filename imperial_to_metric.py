18b44e58b859128343a24d167cadbc2cbfb697ab
Zach Owens <zach@swisswoodworking.com>
2022-02-23 12:06:33 -0800

    added a series of rooms to granito mat lists

--- imperial_to_metric.py
+++ imperial_to_metric.py
@@ -3,7 +3,7 @@ import csv
 """CSV cannot contain special characters '#', '&', etc"""
 
 # Name of csv being imported without '.csv'
-naming_config = 'NESTING-GRANITO X 3 4” VICENZA OAK MATERIAL ORDER'
+naming_config = 'NESTING-GRANITO X 3 4” VICENZA OAK MATERIAL ORDER -'
 # .bSolid file referenced by bNest for tooling information
 # program_name =  'RECTparam.bSolid'
 program_name =  'RECTparam.bSolid'
